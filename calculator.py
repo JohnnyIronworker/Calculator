c63f3be34373354fe131e872d0bedb811eba6519
JohnnyIronworker <94120112+JohnnyIronworker@users.noreply.github.com>
2024-11-14 22:36:32 +0100

    Litle adjustments

--- calculator.py
+++ calculator.py
@@ -4,42 +4,41 @@ Calculator module for performing basic arithmetic operations.
 
 class Calculator:
     """
-    Klasa Calculator służy do wykonywania podstawowych operacji arytmetycznych:
-    dodawanie, odejmowanie, mnożenie i dzielenie.
+    Calculator class for performing basic arithmetic operations.
     """
 
     def __init__(self, op1: float, op2: float):
         """
-        Inicjalizuje dwa operandy.
+        Initialization of the 2 operands
 
-        :param op1: pierwszy operand
-        :param op2: drugi operand
+        :param op1: first operand
+        :param op2: second operand
         """
         self._op1 = op1
         self._op2 = op2
 
     def sum(self) -> float:
         """
-        Zwraca sumę dwóch operandów.
+        Return sum product of two operands.
         """
         return self._op1 + self._op2
 
     def subtract(self) -> float:
         """
-        Zwraca różnicę dwóch operandów.
+        Return subtract product of two operands.
         """
         return self._op1 - self._op2
 
     def multiply(self) -> float:
         """
-        Zwraca iloczyn dwóch operandów.
+        Return multiplied product of two operands.
         """
         return self._op1 * self._op2
 
     def divide(self) -> float:
         """
-        Zwraca iloraz dwóch operandów. Zgłasza błąd przy dzieleniu przez zero.
+        Return divided product of two operands.
         """
         if self._op2 == 0:
-            raise ValueError("Nie można dzielić przez zero")
+            raise ValueError("This is impossible to divide by zero!")
         return self._op1 / self._op2
